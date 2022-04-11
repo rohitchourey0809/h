1e85f9329e294f77b7cba0835399b7e8a43795d2
rohitchourey0809 <rohit1995chourey@gmail.com>
2022-04-11 09:39:29 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ codeevaluation1/src/App.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+function App() {
+  const [score, setscore] = useState(76);
+  const [wicket, setwicket] = useState(2);
+  const [ball, setball] = useState(50);
+  // const []
+
+  return (
+    <div className="App">
+      <h3>India:</h3>
+      <div className="banner">
+        <div>
+          Score:{score}
+          <h1 className="scoreCount">
+            {
+              // show "score" here
+            }
+          </h1>
+        </div>
+        <div>
+          Wicket:{wicket}
+          <h1 className="wicketCount">
+            {
+              // show wicket here
+            }
+          </h1>
+        </div>
+
+        <div>
+          Over:{ball}
+          <h1 className="overCount">
+            {
+              // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball
+              // if 1 more ball is thrown then over is now 5.0
+              // you have to write logic to form this string from current ball number.
+            }
+          </h1>
+        </div>
+      </div>
+
+      <div className="addScore">
+        Add Score
+        {/* these buttons should add the respective amount in the score */}
+        <button
+          className="addScore1"
+          onClick={() => {
+            setscore(score + 1);
+          }}
+        >
+          Add 1
+        </button>
+        <button
+          className="addScore4"
+          onClick={() => {
+            setscore(score + 4);
+          }}
+        >
+          Add 4
+        </button>
+        <button
+          className="addScore6"
+          onClick={() => {
+            setscore(score + 6);
+          }}
+        >
+          Add 6
+        </button>
+      </div>
+
+      <div className="addWicket">
+        Add Wicket
+        {/* Increase the count of wicket */}
+        <button
+          onClick={() => {
+            setwicket(wicket - 1);
+          }}
+        >
+          Add 1 wicket
+        </button>
+      </div>
+
+      <div className="addBall">
+        Add ball
+        {/* Increase the total number of balls thrown here. */}
+        <button
+          onClick={() => {
+            setball(ball + 1);
+          }}
+        >
+          Add 1
+        </button>
+      </div>
+
+      {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+    </div>
+  );
+}
+
+export default App;
